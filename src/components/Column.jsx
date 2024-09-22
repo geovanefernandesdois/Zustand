2cd936daff244beea15b1ed2debd02e66dd12c65
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-09-22 20:34:51 -0300

    feat: :sparkles: Adding new functions to Column and Tasks components

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,18 +1,54 @@
 import "./Column.css";
 import { useStore } from "../store";
 import Task from "./Task";
+import { useState } from "react";
 
 export default function Column({ state }) {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
   );
 
+  const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+
   return (
-    <div className="column">
-      <p>{state}</p>
+    <div
+      className="column"
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDraggedTask(null);
+        console.log(draggedTask);
+      }}
+    >
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
       {tasks.map((task) => (
         <Task title={task.title} key={task.title} />
       ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,27 @@
 import classNames from "classnames";
 import { useStore } from "../store";
 import "./Task.css";
+import trashicon from "../assets/icons8-lixo.svg";
 
 export default function Task({ title }) {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
-
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const deleteTask = useStore((store) => store.deleteTask);
   return (
-    <div className="task">
+    <div
+      className="task"
+      draggable
+      onDragStart={() => {
+        setDraggedTask(task.title);
+      }}
+    >
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <img src={trashicon} onClick={() => deleteTask(task.title)} />
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
