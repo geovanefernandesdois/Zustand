dd4dc03f9e02a8a2549f646572af90397a7bdebe
Geovane Fernandes <geovanefernandesdois@gmail.com>
2024-09-23 17:56:50 -0300

    feat: :sparkles: New features in Column component.

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -2,10 +2,12 @@ import "./Column.css";
 import { useStore } from "../store";
 import Task from "./Task";
 import { useState } from "react";
+import classNames from "classnames";
 
 export default function Column({ state }) {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
 
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
@@ -14,16 +16,23 @@ export default function Column({ state }) {
   const addTask = useStore((store) => store.addTask);
   const setDraggedTask = useStore((store) => store.setDraggedTask);
   const draggedTask = useStore((store) => store.draggedTask);
+  const moveTask = useStore((store) => store.moveTask);
 
   return (
     <div
-      className="column"
+      className={classNames("column", { drop: drop })}
       onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
         e.preventDefault();
       }}
       onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
         setDraggedTask(null);
-        console.log(draggedTask);
       }}
     >
       <div className="titleWrapper">
